5d5d2a4b3526c308ff3f75636e066b9424290f80
GaborEliza <elizagabor01@gmail.com>
2018-06-19 19:39:53 +0300

    News page WIP.

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const ALL_LANGUAGES = [{value: 'ro', image: 'src/images/ro-flag.png', label: 'Dansk'},
+  {value: 'en', image: 'src/images/flag_english.png', label: 'Engelsk'}];
---
+++ src/localization.js
@@ -0,0 +1,16 @@
+import LocalizedStrings from 'react-localization';
+
+export const strings = new LocalizedStrings({
+  ro: {
+    slide1caption: 'Biblioteca organizeaza concurs destinat studentilor',
+    captionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan odio aliquet ex pretium, non elementum risus vestibulum.',
+    slide2caption: 'Bun venit la UAIC!',
+  },
+
+  en: {
+    slide1caption: 'The library organizes a competition for students',
+    captionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan odio aliquet ex pretium, non elementum risus vestibulum.',
+    slide2caption: 'Welcome to UAIC!',
+  },
+
+});
